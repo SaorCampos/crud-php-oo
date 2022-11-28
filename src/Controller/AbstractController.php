859b948cbb651ec4abe3b4c230816ff9ac33646b
SaorCampos <saor93@gmail.com>
2022-11-28 18:24:44 -0300

    tentando fazer o relatorio

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 namespace App\Controller;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
 abstract class AbstractController
 {
     public function render(string $view, ?array $dados = null, bool $navbar = true): void
@@ -14,5 +18,19 @@ abstract class AbstractController
     public function redirect(string $local): void
     {
         header('location: '.$local);
+    }
+    public function relatorio(string $onde, array $dados = []): void
+    {
+        extract($dados);
+        ob_start();
+        include_once('../../Views/{$onde}/relatorio.phtml');
+        $pdf = ob_get_clean();
+        $opcao = new Options();
+        $opcao->set('isRemoteEnabled', true);
+        $dompdf = new Dompdf($opcao);
+        $dompdf->loadHtml($pdf);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream('relatorio.pdf', ['Attachment'=>0,]);
     }
 }
