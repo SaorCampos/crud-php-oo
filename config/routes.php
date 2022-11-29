d9ce41479d21cc60eda6f84e29612a639be12448
SaorCampos <saor93@gmail.com>
2022-11-28 21:39:20 -0300

    criando rotas pra geraar relatorio

--- config/routes.php
+++ config/routes.php
@@ -25,20 +25,24 @@ $rotas = [
     '/usuarios/novo'=> criarRota(UsuarioController::class, 'cadastrar'),
     '/usuarios/editar'=> criarRota(UsuarioController::class, 'editar'),
     '/usuarios/excluir'=> criarRota(UsuarioController::class, 'excluir'),
+    '/usuarios/relatorio'=> criarRota(UsuarioController::class, 'relatorio'),
 
     '/cursos/listar'=> criarRota(CursoController::class, 'listar'),
     '/cursos/novo'=> criarRota(CursoController::class, 'cadastrar'),
     '/cursos/editar'=> criarRota(CursoController::class, 'editar'),
     '/cursos/excluir'=> criarRota(CursoController::class, 'excluir'),
+    '/cursos/relatorio'=> criarRota(CursoController::class, 'relatorio'),
 
     '/professores/listar'=> criarRota(ProfessorController::class, 'listar'),
     '/professores/novo'=> criarRota(ProfessorController::class, 'cadastrar'),
     '/professores/editar'=> criarRota(ProfessorController::class, 'editar'),
     '/professores/excluir'=> criarRota(ProfessorController::class, 'excluir'),
+    '/professores/relatorio'=> criarRota(ProfessorController::class, 'relatorio'),
 
     '/categorias/listar'=> criarRota(CategoriaController::class, 'listar'),
     '/categorias/novo'=> criarRota(CategoriaController::class, 'cadastrar'),
     '/categorias/editar'=> criarRota(CategoriaController::class, 'editar'),
     '/categorias/excluir'=> criarRota(CategoriaController::class, 'excluir'),
+    '/categorias/relatorio'=> criarRota(CategoriaController::class, 'relatorio'),
 ];
 return $rotas;
--- src/Controller/CategoriaController.php
+++ src/Controller/CategoriaController.php
@@ -5,13 +5,18 @@ namespace App\Controller;
 use App\Model\Categoria;
 use Exception;
 use App\Repository\CategoriaRepository;
+use Dompdf\Dompdf;
 
 class CategoriaController extends AbstractController
 {
+    private CategoriaRepository $repository;
+    public function __construct()
+    {
+        $this->repository = new CategoriaRepository();
+    }
     public function listar(): void
     {
-        $rep = new CategoriaRepository();
-        $categorias = $rep->buscarTodos();
+        $categorias = $this->repository->buscarTodos();
         $this->render('categoria/listar', ['categorias'=>$categorias]);
     }
     public function cadastrar(): void
@@ -24,7 +29,7 @@ class CategoriaController extends AbstractController
         $categoria->nome = $_POST['nome'];
         $rep = new CategoriaRepository();
         try{
-            $rep->inserir($categoria);
+            $this->repository->inserir($categoria);
         } catch(Exception $exception){
             if(true === str_contains($exception->getMessage(), 'nome')){
                 die('Categoria já existe');
@@ -36,13 +41,12 @@ class CategoriaController extends AbstractController
     public function editar(): void
     {
         $id = $_GET['id'];
-        $rep = new CategoriaRepository();
-        $categoria = $rep->buscarUm($id);
+        $categoria = $this->repository->buscarUm($id);
         $this->render('categoria/editar', [$categoria]);
         if(false === empty($_POST)){
             $categoria->nome = $_POST['nome'];
             try{
-                $rep->atualizar($categoria, $id);
+                $this->repository->atualizar($categoria, $id);
             }catch(Exception $exception){
                 if(true === str_contains($exception->getMessage(), 'nome')){
                     die('Categoria já existe');
@@ -55,9 +59,47 @@ class CategoriaController extends AbstractController
     public function excluir(): void
     {
         $id = $_GET['id'];
-        $rep = new CategoriaRepository();
-        $rep->excluir($id);
+        $this->repository->excluir($id);
         $this->render('categoria/excluir');
         $this->redirect('/categorias/listar');
+    }
+    private function renderizar(iterable $categorias)
+    {
+        $resultado = '';
+        foreach($categorias as $categoria){
+            $resultado .= "
+            <tr>
+                <td>{$categoria->id}</td>
+                <td>{$categoria->nome}</td>
+            </tr>";
+        }
+        return $resultado;
+    }
+    public function relatorio(): void
+    {
+        $hoje = date('d/m/Y');
+        $categorias = $this->repository->buscarTodos();
+        $design =  "
+            <h1>Relatorio de Alunos</h1>
+            <hr>
+            <em>Gerado em {$hoje}</em>
+            <br>
+            <table border='1' width='100%' style='margin-top: 30px;'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                    </tr>
+                </thead>
+                <tbody>
+                ".$this->renderizar($categorias)."
+                </tbody>
+            </table>
+        ";
+        $dompdf = new Dompdf();
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->loadHtml($design);
+        $dompdf->render();
+        $dompdf->stream('relatorio-categoria.pdf', ['Attachment'=> 0,]);
     }
 }
